0e95a994c5e5485bf794275f6dfb6f26b88d9ccc
SapphicBear <235136735+SapphicBear@users.noreply.github.com>
2026-01-04 19:33:29 +0100

    Make edits to file system to work with modules
    Fix error with linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 module.exports = defineConfig([
-  { files: ["./src/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser} }, globalIgnores(["webpack.*", "dist/"]), eslintConfigPrettier,
+  { files: ["./src/*.{js,mjs,cjs}"], env: {"browser": true, "node": true, "amd": true,}, plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser} }, globalIgnores(["webpack.*", "dist/"]), eslintConfigPrettier,
 ]);
