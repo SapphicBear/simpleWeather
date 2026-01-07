dc8d4570e5b1499b10035c89fec26eab391aa2b1
SapphicBear <sleepyscreenart@gmail.com>
2026-01-07 15:47:15 +0100

    Finish moving code and styling to finish project
    Move DOM code from render module to the index,
    finish styling and finish the project for now!

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import "./style.css";
 import { getData } from "./data.js";
 import { cacheDOM } from "./DOM.js";
 import { render } from "./render.js";
+import { format } from "date-fns";
+import { TZDate } from "@date-fns/tz";
 
 console.log("Javascript connected from Index.js");
 
@@ -16,16 +18,22 @@ function convertUnit() {
     }
     return unit;
 }
+function getTime(data) {
+    let currentTime = new Date()
+    let currentTimeZoned = format(new TZDate(currentTime, `${data.timezone}`), "HH:mm");
+    return currentTimeZoned;
+}
 
 DOM.searchButton.addEventListener("click", () => {
     getData.dataHandler()
         .then((data) => {
             console.log(data)
-            render.samplePrint(data, DOM, convertUnit());
+            render.samplePrint(data, DOM, convertUnit(), getTime(data));
             render.handleBackground(data, DOM);
         }).catch((error) => {
             console.log(error);
             render.printError(error, DOM);
+            render.handleBackground(error, DOM);
         })
 });
 DOM.unitSelector.addEventListener("click", () => {
--- src/render.js
+++ src/render.js
@@ -1,15 +1,11 @@
-import { format } from "date-fns";
-import { TZDate } from "@date-fns/tz";
 export const render = {
-    samplePrint(data, DOM, unit) {
-        let currentTime = new Date()
-        let currentTimeZoned = format(new TZDate(currentTime, `${data.timezone}`), "HH:mm");
+    samplePrint(data, DOM, unit, time) {
         DOM.header.classList.remove("error");
         DOM.header.textContent = `Location: ${data.resolvedAddress}`;
         DOM.headerShort.textContent = `${data.currentConditions.conditions}`;
         DOM.bodyTemp.textContent = `Temp: ${data.currentConditions.temp}${unit} / Feels like: ${data.currentConditions.feelslike}${unit}`;
         DOM.bodyCurrentCon.textContent = `Current conditions: ${data.description}`;
-        DOM.footerTime.textContent = `Time: ${currentTimeZoned}`;
+        DOM.footerTime.textContent = `Time: ${time}`;
         DOM.footerTimeZone.textContent = `Timezone: ${data.timezone}`;
     },
     printError(err, DOM) {
@@ -22,13 +18,16 @@ export const render = {
         DOM.footerTimeZone.textContent = "";
     },
     handleBackground(data, DOM) {
-        const time = data.currentConditions.datetime;
-        if (time > "18:00:00") {
+        try {
+            const time = data.currentConditions.datetime;
+            if (time > "18:00:00") {
             DOM.body.className = "night";
             
-        } else if (time < "18:00:00") {
-            DOM.body.className = "day";
-            
-        } 
+            } else if (time < "18:00:00") {
+                DOM.body.className = "day";
+            } 
+        } catch {
+            DOM.body.className = "";
+        }   
     }
 };
